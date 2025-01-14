295c55a56c193476c1e3f8b8269d3b486a4dbad9
eddyhwang97 <hwangdw1997@gmail.com>
2025-01-14 09:36:03 +0900

    뷰 중간업로드

---
+++ assets/js/product_detail.js
@@ -0,0 +1,11 @@
+// 제품디테일페이지 - product_detail.js
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 3,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
