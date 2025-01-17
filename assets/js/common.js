4ea2424e9fd102ee9dd126b5b993c42fcd958cfe
minji73 <kimminji7373@gmail.com>
2025-01-17 17:57:55 +0900

    faq JS

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,17 @@
 // 공통 JS common.js
 $(() => {
     // tab js
-    const tabListA = $('.tab-list>li>a');
-    const tabView = $('.tab-view>li');
+    const tabList = $('.tab-list>li>a');
+    const tabView = $('.tab-view-list>li');
+    
+    $('.tab-list>li').click(function(){
+        $(this).addClass('on').siblings().removeClass('on');
+    });
 
-    tabListA.on('click', function(){
+    tabList.on('click', function(){
         let tabIdx = $(this).parent().index();
         console.log(tabIdx);
+        tabView.removeClass('on');
+        tabView.eq(tabIdx).addClass('on')
     });
 });
--- assets/js/faq.js
+++ assets/js/faq.js
@@ -1,12 +1,6 @@
 // FAQ JS - faq.js
 
 $( document ).ready(function() {
-    // tab js
-    // 1. 대상선정 : tab-list>li
-    $('.tab-list>li').click(function(){
-        $(this).addClass('on').siblings().removeClass('on');
-    });
-
     // 아코디언 js
     // 대상선정
     // 클릭대상 question , 이벤트대상 : anwser
