54d9ca0fdb55637cfb136fb86b8a572cd5d8e505
mimimgg <com42123@gmail.com>
2025-01-21 14:12:19 +0900

    20250121

--- assets/js/store.js
+++ assets/js/store.js
@@ -1,11 +1,11 @@
 // 6401 팀프로젝트 :: 탬버린즈 - store.js //
 
-const slideWrap = document.querySelector('.store_img');
-const slideImgs = document.querySelectorAll('.store_img img');
-const totalImgs = slideImgs.length;
-let currentSlide = 0;
+const storeImg = document.querySelector(".store_img");
+const slides = document.querySelectorAll(".store_img > img");
 
-// 1. 슬라이드 배너 생성
-function (index){
-  
-};
+/*************************************************************
+  [  요구사항  ]
+  - prev, next 버튼 없이 스와이프로 동작
+  - 배너 개수에 맞게 하단에 바형 인디케이터 설정 및 조작
+*****************************************************************/
+
