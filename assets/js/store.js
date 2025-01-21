05656ea7c4441b2ea119ea4621000503de5d9819
Tom B. Parker <tombap8@hotmail.com>
2025-01-21 15:02:14 +0900

    ss

--- assets/js/store.js
+++ assets/js/store.js
@@ -1,11 +1,32 @@
 // 6401 팀프로젝트 :: 탬버린즈 - store.js //
 
-const storeImg = document.querySelector(".store_img");
-const slides = document.querySelectorAll(".store_img > img");
+const targetEl = document.querySelectorAll('.store_slide_wrap');
 
-/*************************************************************
-  [  요구사항  ]
-  - prev, next 버튼 없이 스와이프로 동작
-  - 배너 개수에 맞게 하단에 바형 인디케이터 설정 및 조작
-*****************************************************************/
+targetEl.forEach(el => setElFn(el));
 
+function setElFn(tgEl){
+  // tgEl - 공통 최상위 클래스 (이것만큼 돌면서 모두 생성함!)
+  const storeOl = tgEl.querySelector(".store_indic ol");
+  const indicBox = tgEl.querySelector(".indic_box");
+  const slides = tgEl.querySelectorAll(".store_img li");
+  
+  /*************************************************************
+    [  요구사항  ]
+    - prev, next 버튼 없이 스와이프로 동작
+    - 배너 개수에 맞게 하단에 바형 인디케이터 설정 및 조작
+  *****************************************************************/
+  
+  slides.forEach((el) => {
+    storeOl.innerHTML += `<li></li>`;
+  });
+  
+  tgEl.querySelectorAll(".store_indic ol li")
+  .forEach((el) => {
+    el.onmouseover = function () {
+      console.log(this);
+      indicBox.style.left = this.offsetLeft + "px";
+    };
+  });
+  indicBox.style.width = 100 / slides.length + "%";
+
+} ////////////// setElFn 함수 /////////////////////
