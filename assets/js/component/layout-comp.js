75ca3b6cc53a766af259055a1cfc83261d2670c8
kimeming <kimeming030@gmail.com>
2025-01-22 10:25:37 +0900

    footer css 수정

--- assets/js/component/layout-comp.js
+++ assets/js/component/layout-comp.js
@@ -123,20 +123,26 @@ export const Footer = {
     <footer class="footer">
             <div class="footer-inner">
                 <div class="footer-top">
-                    <ul class="footer-link">
-                        <li><a href="#">공지사항</a></li>
-                        <li><a href="#">고객서비스</a></li>
-                    </ul>
-                    <ul class="sns-list">
-                        <li><a href="https://www.instagram.com/tamburinsofficial/" target="_blank" title="새창열림">INSTAGRAM</a></li>
-                        <li><a href="https://www.facebook.com/tamburinsofficial/" target="_blank" title="새창열림">FACEBOOK</a></li>
-                        <li><a href="https://pf.kakao.com/_RkqIj" target="_blank" title="새창열림">KAKAOTALK</a></li>
-                        <li><a href="https://weibo.com/tamburinsofficial" target="_blank" title="새창열림">WEIBO</a></li>
-                    </ul>
-                </div>
-                <div class="footer-btm">
                     <div class="footer-logo">
                         <img src="/ASSETS/img/main/logo.svg" alt="TAMBURINS">
+                    </div>
+                </div>
+                <div class="footer-btm">
+                    <div class="left">
+                      <p class="bold">COMMUNITY</p>
+                      <ul class="footer-link">
+                        <li><a href="#">공지사항</a></li>
+                        <li><a href="#">고객서비스</a></li>
+                      </ul>
+                    </div>
+                    <div class="right">
+                      <p class="bold">SNS</p>
+                      <ul class="sns-list">
+                          <li><a href="https://www.instagram.com/tamburinsofficial/" target="_blank" title="새창열림">INSTAGRAM</a></li>
+                          <li><a href="https://www.facebook.com/tamburinsofficial/" target="_blank" title="새창열림">FACEBOOK</a></li>
+                          <li><a href="https://pf.kakao.com/_RkqIj" target="_blank" title="새창열림">KAKAOTALK</a></li>
+                          <li><a href="https://weibo.com/tamburinsofficial" target="_blank" title="새창열림">WEIBO</a></li>
+                      </ul>
                     </div>
                 </div>
             </div>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,98 +1,113 @@
 $(() => {
-    // header intro animation - 첫 진입 시에만
-    if(window.innerWidth >= 1024){
-        const $logo = $('.header .logo');
-        const pcVideo = $('.video-wrap .web');
-        $('.header .util, .header-left').css('display', 'none');
-        pcVideo[0].pause();
-        $('body').addClass('on');
+  // header intro animation - 첫 진입 시에만
+  function headerAni() {
+    if (window.innerWidth >= 1024) {
+      const $logo = $(".header .logo");
+      const pcVideo = $(".video-wrap .web");
+      $(".header .util, .header-left").css("display", "none");
+      pcVideo[0].pause();
+      $("body").addClass("on");
 
-        setTimeout(function() {
-            $logo.addClass('active');
-            pcVideo[0].play();
-            $('.header .util, .header-left').fadeIn();
-            $('body').removeClass('on');
-        }, 500);
+      setTimeout(function () {
+        $logo.addClass("active");
+        pcVideo[0].play();
+        $(".header .util, .header-left").fadeIn();
+        $("body").removeClass("on");
+      }, 500);
     }
+    // scrollTop이 0일때만 실행 조건걸기
+    // header util 안보이게
+    // 첫 진입때만 실행하기
+  }
 
-    // best seller slide
-    const bestSlide = new Swiper('.best-slide', {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        centeredSlides: true,
-        breakpoints: {
-            360: {
-                slidesPerView: 2,
-                spaceBetween: 40,
-            },
-            768: {
-                slidesPerView: 3,
-                spaceBetween: 40,
-            },
-            1024: {
-                slidesPerView: 5,
-                spaceBetween: 50,
-                centeredSlides: false,
-            },
+  headerAni();
+
+  // best seller slide
+  const bestSlide = new Swiper(".best-slide", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    breakpoints: {
+      360: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+        centeredSlides: false,
+      },
+    },
+  });
+
+  // store slide
+  const storeSlide = new Swiper(".store-slide", {
+    slidesPerView: 1,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: false,
+      draggable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    on: {
+      slideChange: function () {
+        const storeArr = [
+          "플래그십 스토어 한남",
+          "플래그십 스토어 성수",
+          "플래그십 스토어 삼청",
+          "하우스 도산",
+          "플래그십 스토어 신사",
+        ];
+        let realIdx = this.realIndex;
+        const storeName = document.querySelector(".store-name");
+
+        // 클래스 초기화
+        storeName.classList.remove("show");
+
+        // 텍스트 추가 후 클래스 재추가
+        setTimeout(() => {
+          storeName.innerText = storeArr[realIdx];
+          storeName.classList.add("show");
+        }, 500);
+      },
+    },
+  });
+
+  // section header change
+  const header = document.querySelector("header"); // header 요소 선택
+  const sections = document.querySelectorAll("section"); // 모든 section 요소 선택
+  const headerHeight = header.offsetHeight; // header의 높이 계산
+
+  window.addEventListener("scroll", () => {
+    let addBlackClass = false;
+
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+
+      // 섹션의 상단이 헤더 높이만큼 보였을 때 조건 확인
+      if (rect.top <= headerHeight && rect.bottom >= headerHeight) {
+        if (
+          section.classList.contains("best") ||
+          section.classList.contains("store")
+        ) {
+          addBlackClass = true;
         }
+      }
     });
 
-    // store slide
-    const storeSlide = new Swiper('.store-slide', {
-        slidesPerView: 1,
-        scrollbar: {
-            el: ".swiper-scrollbar",
-            hide: false,
-            draggable: true,
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        loop: true,
-        on: {
-            slideChange: function () {
-                const storeArr = ['플래그십 스토어 한남', '플래그십 스토어 성수', '플래그십 스토어 삼청', '하우스 도산', '플래그십 스토어 신사'];
-                let realIdx = this.realIndex;
-                const storeName = document.querySelector('.store-name');
-            
-                // 클래스 초기화
-                storeName.classList.remove('show');
-            
-                // 텍스트 추가 후 클래스 재추가
-                setTimeout(() => {
-                    storeName.innerText = storeArr[realIdx];
-                    storeName.classList.add('show');
-                }, 500);
-            }
-            
-        }
-    });
-
-    // section header change
-    const header = document.querySelector('header'); // header 요소 선택
-    const sections = document.querySelectorAll('section'); // 모든 section 요소 선택
-    const headerHeight = header.offsetHeight; // header의 높이 계산
-
-    window.addEventListener('scroll', () => {
-        let addBlackClass = false;
-
-        sections.forEach((section) => {
-            const rect = section.getBoundingClientRect();
-
-            // 섹션의 상단이 헤더 높이만큼 보였을 때 조건 확인
-            if (rect.top <= headerHeight && rect.bottom >= headerHeight) {
-                if (section.classList.contains('best') || section.classList.contains('store')) {
-                    addBlackClass = true;
-                }
-            }
-        });
-
-        // header에 black 클래스 추가/제거
-        if (addBlackClass) {
-            header.classList.add('black');
-        } else {
-            header.classList.remove('black');
-        }
-    });
+    // header에 black 클래스 추가/제거
+    if (addBlackClass) {
+      header.classList.add("black");
+    } else {
+      header.classList.remove("black");
+    }
+  });
 });
