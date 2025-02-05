8cb5d36aaefd0d73f73cca3417ad700814581362
kimeming <kimeming030@gmail.com>
2025-02-05 15:01:27 +0900

    gnb 방식 수정...... ㅠㅠ

--- assets/js/component/sub-comp.js
+++ assets/js/component/sub-comp.js
@@ -1,29 +1,24 @@
 // sub page layout component
-import linkSetData from "/assets/data/gnb-data.js";
 import store from "/assets/js/vuex_store_store.js";
 export const subTop = {
   template: `
       <div class="sub-top">
         <div class="inner">
-          <ul class="category-tab" v-for="(item, key) in linkSetData" :key="key">
+          <ul class="category-tab" v-for="(val,key) in $store.state.linkSetData">
             <h3 class="sub-title">{{key}}</h3>
             <li class="tab" 
-                v-for="(subItem, subKey) in item.menu" :key="subKey"
-                
-                @click="getCatName(subKey)"
-                :class="{on: $store.state.catName === subKey}" 
+                v-for="(v,k) in val.menu" 
+                :key="k"
+                @click="getCatName(k)"
+                :class="{on: $store.state.catName === k}" 
                 >
-                <router-link :to="'/' + subItem.name + '/' + subItem.params.cls">{{subKey}}</router-link>
-            </li> 
+              <a href="#">{{k}}</a>
+            </li>
           </ul>
         </div>
       </div>
     `,
-data(){
-  return {
-    linkSetData,
-  };
-},
+
   methods: {
     getCatName(pm) {
       console.log("선택한 카테고리:", pm);
@@ -32,10 +27,6 @@ data(){
   },
   created() {},
   mounted() {
-    $('.category-tab').each((i,x)=>{
-      console.log(i,x)
-      if($(x).find('.on')) $(x).children('.on').parents().hide();
-    });
   },
 };
 
@@ -51,6 +42,9 @@ export const SubLayout = {
   components: {
     "sub-top-comp": subTop, // 상단 컴포넌트
   },
+  created() {
+    console.log("SubLayout - showSubTop:", this.showSubTop);
+  }
 };
 
 export const Community = {
