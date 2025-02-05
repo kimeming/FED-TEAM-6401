4958440330ca560d0857ba1de652409230c46b8e
gogoeu <ko1331@naver.com>
2025-02-05 10:47:43 +0900

    안에 있는 join-app을 뿌리려고 하니까 안나옴
    안에 있는 join-app을 뿌리려고 하니까 안나옴

--- assets/js/join_vue.js
+++ assets/js/join_vue.js
@@ -5,14 +5,12 @@
 // import { BannerComp } from "./components/banner.js";
 import { JoinComp } from "./component/login-comp.js";
 
-console.log(JoinComp);
-
 /******************************* 
     메인 뷰 인스턴스 생성하기 
 ********************************/
 new Vue({
     // 1. 대상설정
-    el: '#join-app',
+    el: '#main',
     // 2. 데이터설정
     data:{},
     // 3. 메서드설정
